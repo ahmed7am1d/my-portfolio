12fd1a3ee19e56bc744c32d8e46fe314d238663a
ahmed7am1d <ahmed.al-doori@blogic.cz>
2023-02-02 14:40:17 +0300

    First upload

---
+++ components/Header.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { SocialIcon } from 'react-social-icons';
+type Props = {}
+
+function Header({ }: Props) {
+    return (
+        <header className='sticky top-0 p-5 flex items-start justify-between max-w-7xl mx-auto z-10 xl:items-center'>
+            <div className='flex flex-row items-center'>
+                {/* Social icons */}
+                <SocialIcon url="https://twitter.com/aldooriDEV" fgColor="gray" bgColor="transparent" />
+                <SocialIcon url="https://www.instagram.com/ahmad7am1d/" fgColor="gray" bgColor="transparent" />
+                <SocialIcon url="https://github.com/ahmed7am1d" fgColor="gray" bgColor="transparent" />
+            </div>
+
+            <div className='flex flex-row items-center text-gray-300 cursor-pointer'>
+                {/* Nav */}
+                <SocialIcon
+                    className="cursor-pointer"
+                    network="email"
+                    fgColor="gray"
+                    bgColor="transparent"
+                />
+                <p className='uppercase hidden md:inline-flex text-sm text-gray-400 '>Let`s connect</p>
+            </div>
+        </header>
+    )
+}
+
+export default Header
