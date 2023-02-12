9d7a37a63a9ee4d9034051350bb3bcf657a1e104
ahmed7am1d <ahmed.al-doori@blogic.cz>
2023-02-13 00:16:54 +0300

    Updating meta tags to be acceptable by linkedIn

--- pages/index.tsx
+++ pages/index.tsx
@@ -32,13 +32,16 @@ const Home = ({ pageInfo, experiences, projects, skills, socials }: Props) => {
     <>
       <Head>
         <meta charSet="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta
           name="description"
           content="Discover the portfolio of experienced software engineer, Ahmed Hamid Taha Al-Doori. Specializing in developing innovative and efficient software solutions, Al-Doori's portfolio showcases a range of diverse projects that demonstrate his expertise and passion for coding, Al-Doori's work showcases his technical skills and ability to create solutions that meet clients' needs. Explore the portfolio now at al-doori.ml and see how Al-Doori can help bring your software vision to life."
         />
         <meta name="author" content="Ahmed Hamid Al-Doori" />
         <meta name="keywords" content="Software engineer, Web developer, ReactJS, NextJS " />
+        {/* For LinkedIn */}
+        <meta property='og:description' content="Discover the portfolio of experienced software engineer, Ahmed Hamid Taha Al-Doori. Specializing in developing innovative and efficient software solutions, Al-Doori's portfolio showcases a range of diverse projects that demonstrate his expertise and passion for coding, Al-Doori's work showcases his technical skills and ability to create solutions that meet clients' needs. Explore the portfolio now at al-doori.ml and see how Al-Doori can help bring your software vision to life." />
+        
       </Head>
       <div className='bg-primaryColorDark text-white h-screen snap-y snap-mandatory overflow-y-scroll overflow-x-hidden z-0 
       scroll-smooth
